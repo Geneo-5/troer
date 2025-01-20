9220e444fa9b938173e086c39de39b70b1c2a66d
Loïc Jourdheuil Sellin <46419549+Geneo-5@users.noreply.github.com>
2025-01-20 10:30:31 +0100

    Change tab to space

--- src/troer/main.py
+++ src/troer/main.py
@@ -5,74 +5,74 @@ from argparse import ArgumentParser
 from .spec import *
 
 available_mode = {
-	"lib-header":    {"", ".h"},
-	"lib-src":       {"", ".c"},
-	#	"json-header":   {"json-", ".h"},
-	#	"json-src":      {"json-", ".c"},
-	#	"user-header":   {"user-", ".h"},
-	#	"user-src":      {"user-", ".c"},
-	#	"server-header": {"srv-", ".h"},
-	#	"server-src":    {"srv-", ".c"},
+    "lib-header":    {"", ".h"},
+    "lib-src":       {"", ".c"},
+    #	"json-header":   {"json-", ".h"},
+    #	"json-src":      {"json-", ".c"},
+    #	"user-header":   {"user-", ".h"},
+    #	"user-src":      {"user-", ".c"},
+    #	"server-header": {"srv-", ".h"},
+    #	"server-src":    {"srv-", ".c"},
 }
 
 def build_mode(parsed, mode, out_file, cmp_out):
-	out = parsed.rendering(mode)
+    out = parsed.rendering(mode)
 
-	if not out_file:
-		print(out)
-		return
+    if not out_file:
+        print(out)
+        return
 
-	if cmp_out:
-		try:
-			with open(out_file, 'r') as f:
-				data = f.read()
-			if data == out:
-				return
-		except:
-			pass
+    if cmp_out:
+        try:
+            with open(out_file, 'r') as f:
+                data = f.read()
+            if data == out:
+                return
+        except:
+            pass
 
-		with open(out_file, 'w') as f:
-			f.write(out)
+        with open(out_file, 'w') as f:
+            f.write(out)
 
 def cli():
-	parser = ArgumentParser(description='IDL translatore to dpack serializer')
-	parser.add_argument('--spec', dest='spec', type=str, required=True)
-	parser.add_argument('--mode', dest='mode', type=str, default=None)
-	parser.add_argument('--all', action='store_true', default=None, help='Build all modes')
-	parser.add_argument('--cmp-out', action='store_true', default=None,
-		help='Do not overwrite the output file if the new output is identical to the old')
-	parser.add_argument('-o', dest='out_file', type=str, default=None)
-	args = parser.parse_args()
+    parser = ArgumentParser(description='IDL translatore to dpack serializer')
+    parser.add_argument('--spec', dest='spec', type=str, required=True)
+    parser.add_argument('--mode', dest='mode', type=str, default=None)
+    parser.add_argument('--all', action='store_true', default=None, help='Build all modes')
+    parser.add_argument('--cmp-out', action='store_true', default=None,
+        help='Do not overwrite the output file if the new output is identical to the old')
+    parser.add_argument('-o', dest='out_file', type=str, default=None)
+    args = parser.parse_args()
 
-	if (args.all and  args.mode) or not (args.all or  args.mode):
-		print("Choice --all xor --mode MODE", file=stderr)
-		return EX_USAGE
+    if (args.all and  args.mode) or not (args.all or  args.mode):
+        print("Choice --all xor --mode MODE", file=stderr)
+        return EX_USAGE
 
-	if args.mode and args.mode not in available_mode:
-		print(f"Bad mode {args.mode} must be in [{available_mode.keys()}]", file=stderr)
-		return EX_USAGE
+    if args.mode and args.mode not in available_mode:
+        print(f"Bad mode {args.mode} must be in [{available_mode.keys()}]", file=stderr)
+        return EX_USAGE
 
-	try:
-		parsed = SpecFamily(args.spec)
-	except Exception as e:
-		print(e)
-		return EX_DATAERR
-	try:
-		if args.mode:
-			build_mode(parsed, args.mode, args.out_file, args.cmp_out)
-		elif args.all:
-			for m in available_mode.keys():
-				f = None
-				if args.out_file:
-					prefix, ext = available_mode[m]
-					f = f"{args.out_file}/{prefix}{parsed.name}.{ext}"
-				build_mode(parsed, m, f, args.cmp_out)
-	except Exception as e:
-		print(e)
-		return EX_IOERR
+    try:
+        parsed = SpecFamily(args.spec)
+    except Exception as e:
+        print(e)
+        return EX_DATAERR
+    try:
+        if args.mode:
+            build_mode(parsed, args.mode, args.out_file, args.cmp_out)
+        elif args.all:
+            for m in available_mode.keys():
+                f = None
+                if args.out_file:
+                    prefix, ext = available_mode[m]
+                    f = f"{args.out_file}/{prefix}{parsed.name}.{ext}"
+                build_mode(parsed, m, f, args.cmp_out)
+    except Exception as e:
+        print(e)
+        return EX_IOERR
 
-	return EX_OK
+    return EX_OK
 
 if __name__ == "__main__":
-	exit(cli())
+    exit(cli())
 
--- src/troer/spec/spec.py
+++ src/troer/spec/spec.py
@@ -8,138 +8,138 @@ from troer import schemas
 from troer import templates
 
 def newElement(name):
-	return globals()[f"Spec{name.title()}"]
+    return globals()[f"Spec{name.title()}"]
 
 def get_align(elems):
-	return max([e.get_name().len for e in elems])
+    return max([e.get_name().len for e in elems])
 
 class SpecElement:
-	def __init__(self, family, yaml):
-		self.family = family
-		self.yaml = yaml
+    def __init__(self, family, yaml):
+        self.family = family
+        self.yaml = yaml
 
-		if 'name' in self.yaml:
-			self.name = self.yaml['name']
-			self.ident_name = self.name.replace('-', '_')
+        if 'name' in self.yaml:
+            self.name = self.yaml['name']
+            self.ident_name = self.name.replace('-', '_')
 
-		self._super_resolved = False
-		family.add_unresolved(self)
+        self._super_resolved = False
+        family.add_unresolved(self)
 
-	def __getitem__(self, key):
-		return self.yaml[key]
+    def __getitem__(self, key):
+        return self.yaml[key]
 
-	def __contains__(self, key):
-		return key in self.yaml
+    def __contains__(self, key):
+        return key in self.yaml
 
-	def get(self, key, default=None):
-		return self.yaml.get(key, default)
+    def get(self, key, default=None):
+        return self.yaml.get(key, default)
 
-	def get_name(self):
-		return self.family.prefix + self.ident_name
+    def get_name(self):
+        return self.family.prefix + self.ident_name
 
-	def resolve_up(self, up):
-		if not self._super_resolved:
-			up.resolve()
-			self._super_resolved = True
+    def resolve_up(self, up):
+        if not self._super_resolved:
+            up.resolve()
+            self._super_resolved = True
 
-	def resolve(self):
-		pass
+    def resolve(self):
+        pass
 
-	def rendering(self, mode):
-		raise Exception("Cannot randering SpecElement")
+    def rendering(self, mode):
+        raise Exception("Cannot randering SpecElement")
 
 class ConstElement(SpecElement):
-	def __init__(self, family, yaml):
-		super().__init__(family, yaml)
-		self.hidden = False
-		if 'header' in yaml:
-			family.add_header(yaml['header'])
-			self.hidden = True
+    def __init__(self, family, yaml):
+        super().__init__(family, yaml)
+        self.hidden = False
+        if 'header' in yaml:
+            family.add_header(yaml['header'])
+            self.hidden = True
 
 class SpecConst(ConstElement):
-	def __init__(self, family, yaml):
-		super().__init__(family, yaml)
+    def __init__(self, family, yaml):
+        super().__init__(family, yaml)
 
 class SpecEnum(ConstElement):
-	def __init__(self, family, yaml):
-		super().__init__(family, yaml)
+    def __init__(self, family, yaml):
+        super().__init__(family, yaml)
 
 class SpecFlags(ConstElement):
-	def __init__(self, family, yaml):
-		super().__init__(family, yaml)
+    def __init__(self, family, yaml):
+        super().__init__(family, yaml)
 
 class SpecRef(ConstElement):
-	def __init__(self, family, yaml):
-		super().__init__(family, yaml)
+    def __init__(self, family, yaml):
+        super().__init__(family, yaml)
 
 class SpecFamily(SpecElement):
-	def __init__(self, spec_path):
-		with open(spec_path, "r") as f:
-			spec = safe_load(f)
-		self.schema = spec.get("schema", None)
-		if not self.schema:
-			raise YAMLError("Missing schema definition")
-		# with open_text("troer", "schema", f"{self.schema}.yaml", encoding="utf-8") as f:
-		with open_text(schemas, f"{self.schema}.yaml") as f:
-			schema = safe_load(f)
-		validate(spec, schema)
+    def __init__(self, spec_path):
+        with open(spec_path, "r") as f:
+            spec = safe_load(f)
+        self.schema = spec.get("schema", None)
+        if not self.schema:
+            raise YAMLError("Missing schema definition")
+        # with open_text("troer", "schema", f"{self.schema}.yaml", encoding="utf-8") as f:
+        with open_text(schemas, f"{self.schema}.yaml") as f:
+            schema = safe_load(f)
+        validate(spec, schema)
 
-		self._resolution_list = []
-		super().__init__(self, spec)
+        self._resolution_list = []
+        super().__init__(self, spec)
 
-		self.header = set()
-		self.defines = OrderedDict()
-		self.enums = OrderedDict()
+        self.header = set()
+        self.defines = OrderedDict()
+        self.enums = OrderedDict()
 
-		last_exception = None
-		while len(self._resolution_list) > 0:
-			resolved = []
-			unresolved = self._resolution_list
-			self._resolution_list = []
+        last_exception = None
+        while len(self._resolution_list) > 0:
+            resolved = []
+            unresolved = self._resolution_list
+            self._resolution_list = []
 
-			for elem in unresolved:
-				try:
-					elem.resolve()
-				except (KeyError, AttributeError) as e:
-					self._resolution_list.append(elem)
-					last_exception = e
-					continue
-				resolved.append(elem)
+            for elem in unresolved:
+                try:
+                    elem.resolve()
+                except (KeyError, AttributeError) as e:
+                    self._resolution_list.append(elem)
+                    last_exception = e
+                    continue
+                resolved.append(elem)
 
-			if len(resolved) == 0:
-				raise last_exception
+            if len(resolved) == 0:
+                raise last_exception
 
-	def add_header(self, header):
-		self.header.add(header)
+    def add_header(self, header):
+        self.header.add(header)
 
-	def add_define(self, name, define):
-		self.defines[name] = define
-	
-	def add_enum(self, name, enum):
-		self.enums[name] = enum
+    def add_define(self, name, define):
+        self.defines[name] = define
 
-	def add_unresolved(self, elem):
-		self._resolution_list.append(elem)
+    def add_enum(self, name, enum):
+        self.enums[name] = enum
 
-	def _resolve_definitions(self):
-		for elem in self.yaml.get('definitions', []):
-			newElement(elem['type'])(self, elem)
+    def add_unresolved(self, elem):
+        self._resolution_list.append(elem)
 
-	def _resolve_attribute(self):
-		pass
+    def _resolve_definitions(self):
+        for elem in self.yaml.get('definitions', []):
+            newElement(elem['type'])(self, elem)
 
-	def _resolve_operations(self):
-		pass
+    def _resolve_attribute(self):
+        pass
 
-	def resolve(self):
-		self.resolve_up(super())
-		self.prefix = f"{self.yaml.get('prefix', self.ident_name)}_"
-		self._resolve_definitions()
-		self._resolve_attribute()
-		if self.schema == "exchange":
-			self._resolve_operations()
+    def _resolve_operations(self):
+        pass
 
-	def rendering(self, mode):
-		tmpl = read_text(templates, f"{mode}.tmpl")
-		return Template(tmpl, self)
+    def resolve(self):
+        self.resolve_up(super())
+        self.prefix = f"{self.yaml.get('prefix', self.ident_name)}_"
+        self._resolve_definitions()
+        self._resolve_attribute()
+        if self.schema == "exchange":
+            self._resolve_operations()
+
+    def rendering(self, mode):
+        tmpl = read_text(templates, f"{mode}.tmpl")
+        return Template(tmpl, self)
 
